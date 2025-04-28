706977634351bdde3427f341bcc0dac7a4e1b18c
monge <ojdupool2004@mail.ru>
2025-04-28 19:24:20 +0300

    логика контрактов, рекламной кампании

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product.apps.ProductConfig',
-    'ads.apps.AdsConfig'
+    'ads.apps.AdsConfig',
+    'contracts.apps.ContractsConfig'
 ]
 
 MIDDLEWARE = [
